de3ae67b27b7437b10d98bd06f6f9a01b27abfdb
Thedax-BI <thedax.bi@gmail.com>
2025-08-15 21:09:36 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Defina aqui a URL do seu Worker após publicar (ex.: https://psi-search.your-subdomain.workers.dev)
-export const AGGREGATOR_BASE = "https://SEU-WORKER.workers.dev";
+export const AGGREGATOR_BASE = "https://thedax-bi.github.io/psi-search/";
